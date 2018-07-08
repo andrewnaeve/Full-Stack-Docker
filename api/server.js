6aed70e1db53faf700653197b28f25790d9ba165
andrewnaeve <andrewnaeve@gmail.com>
2018-07-08 00:35:12 -0700

    upgrade apollo

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const numCPUs = require('os').cpus().length;
 
 const startServer = () => {
   const server = new ApolloServer({ schema, context });
-  server.listen({ http: { port: 8000 } }).then(({ url }) => {
+  server.listen({ port: 8000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
   });
 };
